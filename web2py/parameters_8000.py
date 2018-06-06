d5cc299cc6b3eb55a19550841d64f24323519a2b
Matias Portales <matiiportales@gmail.com>
2018-06-05 23:57:16 -0300

    Creo vista listar_socios del controlador actualizaciones.py

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$9466147091458f35$e07ded603dac1a8bf69dc266eb76bfc0cbd851f1"
+password="pbkdf2(1000,20,sha512)$b09cc4ad27a9dd99$f681f88b9ebe801c44a0bd767a9d2a3eaa99f6dd"
