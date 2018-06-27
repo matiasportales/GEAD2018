8dde13607785f8af02313d686efcb6536430c9d7
Matias Portales <matiiportales@gmail.com>
2018-06-26 21:12:19 -0300

    Subo vista modificar y borrar actividad

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$b3e2aae88870eafa$6f517d0cda0d28aab06c37c9c238a5e8dbf7486e"
+password="pbkdf2(1000,20,sha512)$8b9b03f03a8c2422$c4edbf8d3e040c4f8f89d510ad6d1b4d8cd28c6a"
